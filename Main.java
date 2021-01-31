575640678f482e427a4b423d23aa9d97f2036dc0
Kyle McDowell <kymcd21@icstudents.org>
2021-01-31 18:34:53 +0000

    final

--- Main.java
+++ Main.java
@@ -8,29 +8,31 @@ public class Main {
     quiz[0] = new Question("What's wrong with the creator of this program?");
     quiz[0].setAnswer("mental illness");
 
-   quiz[1]=new ChoiceQuestion("Is it OK to be happy during corona?");
-    quiz[1].addChoice("no", false);
-    quiz[1].addChoice("yes", true);
+    ChoiceQuestion question = new ChoiceQuestion("Is it OK to be happy during corona?");
+    question.addChoice("no", false);
+    question.addChoice("yes", true);
 
-    quiz[2] = question;
+    quiz[1] = question;
     NumericQuestion questionButNumbers = new NumericQuestion("What's 9+10?");
     questionButNumbers.setAnswer(21);
+    quiz[2] = questionButNumbers;
     Scanner in = new Scanner(System.in);
+    double totalQuestions = 3;
+    double qCorrect = 0;
+
     for (Question q : quiz) {
       q.display();
       System.out.print("Your answer: ");
       String response = in.nextLine();
       System.out.println(q.checkAnswer(response));
+      if (q.checkAnswer(response) == true) {
+        qCorrect++;
+      } else {
+
+      }
     }
+
+    System.out.println("You got " + qCorrect + " out of " + totalQuestions + " correct. That's "
+        + (qCorrect / totalQuestions)*100 + " correct.");
   }
 }
-/*   double totalQuestions=3;
-    double qCorrect=0;
-    if(qBuzz.checkAnswer(response)==true){
-      qCorrect++;
-    }else{
-
-    }
-    System.out.println("You got "+qCorrect+" out of "+totalQuestions+" correct. That's "+qCorrect/totalQuestions+" correct.");
-  }
-  */
